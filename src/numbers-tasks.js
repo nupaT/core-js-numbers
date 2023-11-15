e864baccf7886afbf5553f3b6959b57acd80bcef
Evgeni Anisoev <trash_jack@mail.ru>
2023-11-15 21:49:26 +0300

    solve 39 tasks

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -477,8 +477,8 @@ function isInteger(number) {
  * '4.567abcdefgh' => 4.567
  * 'abcdefgh'      => NaN
  */
-function getFloatOnString(/* str */) {
-  throw new Error('Not implemented');
+function getFloatOnString(str) {
+  return Number.parseFloat(str);
 }
 
 /**
@@ -495,8 +495,11 @@ function getFloatOnString(/* str */) {
  * '1.234', 2           => 1
  * '10', 8              => 8
  */
-function getIntegerOnString(/* str, base */) {
-  throw new Error('Not implemented');
+function getIntegerOnString(str, base) {
+  if (Number.parseInt(str, base)) {
+    return Number.parseInt(str, base);
+  }
+  return NaN;
 }
 
 /**
